db617cb7985757234488fdeb0e8b9ea2ae9dbd65
augustojs1 <augustojsouza1@gmail.com>
2026-05-30 12:45:05 -0300

    feat: add RequestLoggingFilter class to log incoming HTTP requests.

---
+++ src/main/java/br/com/augustojsdev/stagepass/infra/filters/RequestLoggingFilter.java
@@ -0,0 +1,24 @@
+package br.com.augustojsdev.stagepass.infra.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Log4j2
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        log.info("Request to -> {} {}", request.getMethod(), request.getRequestURI());
+    }
+}
